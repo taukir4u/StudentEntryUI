0ab9bfc20f60142b139f2b2e5790c6994fd82636
taukir4u <taukir4u@gmail.com>
2014-12-27 10:54:25 +0600

    C#
    Signed-off-by: taukir4u <taukir4u@gmail.com>

---
+++ StudentEntryUI/StudentEntryUI.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace StudentEntryUI
+{
+    partial class StudentEntryUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.saveButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.nameTextBox = new System.Windows.Forms.TextBox();
+            this.addressTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.showButton = new System.Windows.Forms.Button();
+            this.listView = new System.Windows.Forms.ListView();
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader8 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(65, 155);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(148, 23);
+            this.saveButton.TabIndex = 3;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Address";
+            // 
+            // nameTextBox
+            // 
+            this.nameTextBox.Location = new System.Drawing.Point(65, 33);
+            this.nameTextBox.Name = "nameTextBox";
+            this.nameTextBox.Size = new System.Drawing.Size(400, 20);
+            this.nameTextBox.TabIndex = 0;
+            // 
+            // addressTextBox
+            // 
+            this.addressTextBox.Location = new System.Drawing.Point(65, 69);
+            this.addressTextBox.Name = "addressTextBox";
+            this.addressTextBox.Size = new System.Drawing.Size(400, 20);
+            this.addressTextBox.TabIndex = 1;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(65, 107);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.Size = new System.Drawing.Size(400, 20);
+            this.emailTextBox.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Email";
+            // 
+            // showButton
+            // 
+            this.showButton.Location = new System.Drawing.Point(290, 155);
+            this.showButton.Name = "showButton";
+            this.showButton.Size = new System.Drawing.Size(175, 23);
+            this.showButton.TabIndex = 4;
+            this.showButton.Text = "Show";
+            this.showButton.UseVisualStyleBackColor = true;
+            this.showButton.Click += new System.EventHandler(this.showButton_Click);
+            // 
+            // listView
+            // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8});
+            this.listView.Location = new System.Drawing.Point(20, 222);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(478, 153);
+            this.listView.TabIndex = 5;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Name";
+            this.columnHeader6.Width = 131;
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Address";
+            this.columnHeader7.Width = 151;
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Email";
+            this.columnHeader8.Width = 104;
+            // 
+            // StudentEntryUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(510, 387);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.showButton);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.addressTextBox);
+            this.Controls.Add(this.nameTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.saveButton);
+            this.Name = "StudentEntryUI";
+            this.Text = "StudentEntryUI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox nameTextBox;
+        private System.Windows.Forms.TextBox addressTextBox;
+        private System.Windows.Forms.TextBox emailTextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button showButton;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.ColumnHeader columnHeader7;
+        private System.Windows.Forms.ColumnHeader columnHeader8;
+    }
+}
+
---
+++ StudentEntryUI/StudentEntryUI.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentEntryUI
+{
+    public partial class StudentEntryUI : Form
+    {
+        string connectionString = @"Server=BITM-401-PC04; Database=UniversityDB; Integrated Security=True";
+
+
+        public StudentEntryUI()
+        {
+            InitializeComponent();
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            string name = nameTextBox.Text;
+            string address = addressTextBox.Text;
+            string email = emailTextBox.Text;
+            
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            string query = String.Format("INSERT INTO tStudent VALUES('{0}','{1}','{2}')", name, address, email);
+
+            SqlCommand command = new SqlCommand(query, connection);
+
+            connection.Open();
+            int rowAffected = command.ExecuteNonQuery();
+            connection.Close();
+
+            if (rowAffected > 0)
+            {
+                MessageBox.Show("Inserted Successfully!");
+            }
+            else
+            {
+                MessageBox.Show("Insert Failed!");
+            }
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            Clear();
+            string sql = "Select * FROM tStudent";
+            SqlConnection cnn = new SqlConnection(connectionString);
+            cnn.Open();
+            SqlCommand cmd = new SqlCommand(sql, cnn);
+            SqlDataReader Reader = cmd.ExecuteReader();
+
+            listView.Items.Clear();
+
+            while (Reader.Read())
+            {
+
+                ListViewItem lv = new ListViewItem(Reader.GetString(1));
+                lv.SubItems.Add(Reader.GetString(2));
+                lv.SubItems.Add(Reader.GetString(3));
+                listView.Items.Add(lv);
+
+
+            }
+
+            Reader.Close();
+            cnn.Close();
+
+        }
+
+        public void Clear()
+        {
+            nameTextBox.Text = "";
+            addressTextBox.Text = "";
+            emailTextBox.Text = "";
+        }
+
+
+    }
+}
